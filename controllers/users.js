c10659350f99d28b366a45eb02dc91a0be099425
serega4517 <serega4517@yandex.ru>
2022-05-18 15:11:44 +0300

    fix: fix test-errors in controllers(users)

--- controllers/users.js
+++ controllers/users.js
@@ -10,12 +10,15 @@ const getUserById = (req, res) => {
   User.findById(req.params.userId)
     .then((user) => {
       if (!user) {
-        return res.status(404).send({ message: 'Пользователь не найден' });
+        return res.status(404).send({ message: 'Пользователь по указанному id не найден' });
       }
       return res.send({ data: user });
     })
-    .catch(() => {
-      res.status(500).send({ message: 'Произошла ошибка' });
+    .catch((err) => {
+      if (err.name === 'CastError') {
+        return res.status(400).send({ message: 'Передан невалидный id пользователя' });
+      }
+      return res.status(500).send({ message: 'Произошла ошибка' });
     });
 };
 
@@ -24,18 +27,24 @@ const createUser = (req, res) => {
 
   User.create({ name, about, avatar })
     .then((user) => res.send({ data: user }))
-    .catch(() => {
-      res.status(500).send({ message: 'Произошла ошибка' });
+    .catch((err) => {
+      if (err.name === 'ValidationError') {
+        return res.status(400).send({ message: 'При создании нового пользователя переданы невалидные данные' });
+      }
+      return res.status(500).send({ message: 'Произошла ошибка' });
     });
 };
 
 const updateUserInfo = (req, res) => {
   const { name, about } = req.body;
 
-  User.findByIdAndUpdate(req.user._id, { name, about }, { new: true })
+  User.findByIdAndUpdate(req.user._id, { name, about }, { runValidators: true, new: true })
     .then((user) => res.send({ data: user }))
-    .catch(() => {
-      res.status(500).send({ message: 'Произошла ошибка' });
+    .catch((err) => {
+      if (err.name === 'ValidationError') {
+        return res.status(400).send({ message: 'При обновлении данных пользователя переданы невалидные данные' });
+      }
+      return res.status(500).send({ message: 'Произошла ошибка' });
     });
 };
 
