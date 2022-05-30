19d1e00a378414a82ffdbd09ebcbea0964be4fdd
serega4517 <serega4517@yandex.ru>
2022-05-30 14:44:38 +0300

    removed the required property for name and about fields

--- models/user.js
+++ models/user.js
@@ -5,14 +5,12 @@ const bcrypt = require('bcryptjs');
 const userSchema = new mongoose.Schema({
   name: {
     type: String,
-    required: true,
     minlength: 2,
     maxlength: 30,
     default: 'Жак-Ив Кусто',
   },
   about: {
     type: String,
-    required: true,
     minlength: 2,
     maxlength: 30,
     default: 'Исследователь',
